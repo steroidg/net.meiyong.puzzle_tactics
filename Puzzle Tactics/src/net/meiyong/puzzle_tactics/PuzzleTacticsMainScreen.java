c57970a803374ec4045a1388258c30b6131f752d
Dinan Yin <steroidg@gmail.com>
2012-11-11 02:13:48 +1100

    Thread now works and renders correctly onto the Canvas.

--- Puzzle Tactics/src/net/meiyong/puzzle_tactics/PuzzleTacticsMainScreen.java
+++ Puzzle Tactics/src/net/meiyong/puzzle_tactics/PuzzleTacticsMainScreen.java
@@ -2,41 +2,60 @@ package net.meiyong.puzzle_tactics;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class PuzzleTacticsMainScreen extends SurfaceView {
+public class PuzzleTacticsMainScreen extends SurfaceView implements SurfaceHolder.Callback {
 	
 	private static final String TAG = PuzzleTacticsMainScreen.class.getSimpleName();
-	/*
-	private Display display = ((Activity) getContext()).getWindowManager().getDefaultDisplay();
-	@SuppressWarnings("deprecation")
-	private int width = display.getWidth();
-	@SuppressWarnings("deprecation")
-	private int height = display.getHeight();
-	*/
 	private RoundButton rbutton;
-	private Canvas canvas;
 	private PuzzleTacticsMainThread mainThread;
-	
-	
 	
 	public PuzzleTacticsMainScreen (Context context) {
 		super (context);
+		SurfaceHolder surfaceHolder = getHolder();
+		Log.d(TAG, "Constructor");
+		
+		
+		// Get the callback of this surfaceHolder, surfaceCreated won't be called if this is not done.
+		surfaceHolder.addCallback(this);
 		rbutton = new RoundButton (50, 50, 100, 50);
 		mainThread = new PuzzleTacticsMainThread(getHolder(), this);
+		setFocusable(true);
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		Log.d(TAG, "onTouchEvent");
-		canvas = null;
-		canvas = getHolder().lockCanvas();
-		canvas.drawColor(Color.BLACK);
-		rbutton.draw(canvas);
-		getHolder().unlockCanvasAndPost(canvas);
 		return true;
+	}
+	
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		mainThread.setRunning(true);
+		mainThread.start();
+	}
+	
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+	}
+	
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		boolean retry = true;
+		while (retry) {
+			try {
+				mainThread.join();
+				retry = false;
+			} catch (InterruptedException e) {
+				// try again shutting down the thread
+			}
+		}
+	}
+	
+	protected void render(Canvas canvas) {
+		rbutton.draw(canvas);
 	}
 }
--- Puzzle Tactics/src/net/meiyong/puzzle_tactics/PuzzleTacticsMainThread.java
+++ Puzzle Tactics/src/net/meiyong/puzzle_tactics/PuzzleTacticsMainThread.java
@@ -3,6 +3,9 @@
  */
 package net.meiyong.puzzle_tactics;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.SurfaceHolder;
 
 /**
@@ -12,12 +15,35 @@ import android.view.SurfaceHolder;
  */
 public class PuzzleTacticsMainThread extends Thread {
 	
+	private static final String TAG = PuzzleTacticsMainThread.class.getSimpleName();
 	private SurfaceHolder surfaceHolder;
 	private PuzzleTacticsMainScreen mainScreen;
+	private boolean running;
 
 	public PuzzleTacticsMainThread (SurfaceHolder surfaceHolder, PuzzleTacticsMainScreen mainScreen) {
+		super();
+		Log.d(TAG, "Constructor");
 		this.surfaceHolder = surfaceHolder;
 		this.mainScreen = mainScreen;
 	}
 	
+	@Override
+	public void run() {
+		Log.d(TAG, "run");
+		Canvas canvas;
+		Log.d(TAG, "run2");
+		while (this.running) {
+			Log.d(TAG, "running");
+			canvas = null;
+			canvas = this.surfaceHolder.lockCanvas();
+			canvas.drawColor(Color.BLACK);
+			mainScreen.render(canvas);
+			this.surfaceHolder.unlockCanvasAndPost(canvas);
+		}
+		Log.d(TAG, "run3");
+	}
+	 
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
 }
