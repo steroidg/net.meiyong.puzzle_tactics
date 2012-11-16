dff94c98905868315c1a7b2dfd4a1e8802e868da
Dinan Yin <steroidg@gmail.com>
2012-11-16 23:58:29 +1100

    Got rid of verbose logs

--- Puzzle Tactics/src/net/meiyong/puzzle_tactics/SidePanel.java
+++ Puzzle Tactics/src/net/meiyong/puzzle_tactics/SidePanel.java
@@ -17,7 +17,7 @@ import android.view.MotionEvent;
  */
 public class SidePanel extends Container{
 
-	private static final String TAG = RoundButton.class.getSimpleName();
+	private static final String TAG = SidePanel.class.getSimpleName();
 	private Paint paint = new Paint();
 	private RectF rectf;
 	private RoundButton closeButton;
@@ -25,6 +25,7 @@ public class SidePanel extends Container{
 	
 	public SidePanel (int x, int y, int w, int h, PuzzleTacticsMainScreen mainScreen) {
 		super (x, y, w, h, mainScreen);
+		Log.d(TAG, "SidePanel constructor x=" + x + " y=" + y + " w=" + w + " h=" + h);
 		rectf = new RectF();
 		closeButton = new RoundButton (x, h/8, w, h/4);
 		endTurnButton = new RoundButton (x, 3*(h/8), w, h/4);
@@ -35,14 +36,15 @@ public class SidePanel extends Container{
 			if (event.getAction() == MotionEvent.ACTION_DOWN) {
 			Log.d(TAG, "Coords: x=" + event.getX() + ",y=" + event.getY());
 			
+			// This is the endTurn button
 			if ((event.getX() < (endTurnButton.getX() + endTurnButton.getWidth()/2) &&
 					event.getX() > (endTurnButton.getX() - endTurnButton.getWidth()/2)) && 
 					(event.getY() < (endTurnButton.getY() + endTurnButton.getHeight()/2) &&
 							event.getY() > (endTurnButton.getY() - endTurnButton.getHeight()/2))) {
 				mainScreen.newTurn();
 			}
-			// This is the close button
 			
+			// This is the close button
 			if ((event.getX() < (closeButton.getX() + closeButton.getWidth()/2) &&
 					event.getX() > (closeButton.getX() - closeButton.getWidth()/2)) && 
 					(event.getY() < (closeButton.getY() + closeButton.getHeight()/2) &&
@@ -60,7 +62,6 @@ public class SidePanel extends Container{
 	
 	@Override
 	public void draw (Canvas canvas) {
-		Log.d(TAG, "x=" + x + " y=" + y + " w=" + w + " h=" + h);
 		paint.setColor(Color.BLUE);
 		rectf.top = y - h/2;
 		rectf.bottom = y + h/2;
