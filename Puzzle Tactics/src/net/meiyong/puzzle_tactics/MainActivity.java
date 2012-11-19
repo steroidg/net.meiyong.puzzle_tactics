1fc21eb1ecec516f950f419e3c7ba2dd0db2fdaf
Dinan Yin <steroidg@gmail.com>
2012-11-19 22:04:00 +1100

    force the display to be horizontal

--- Puzzle Tactics/src/net/meiyong/puzzle_tactics/MainActivity.java
+++ Puzzle Tactics/src/net/meiyong/puzzle_tactics/MainActivity.java
@@ -1,6 +1,7 @@
 package net.meiyong.puzzle_tactics;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
@@ -19,6 +20,8 @@ public class MainActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		//set window to full screen
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		//force the display to be horizontal
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		setContentView(new PuzzleTacticsMainScreen(this));
 	}
 
