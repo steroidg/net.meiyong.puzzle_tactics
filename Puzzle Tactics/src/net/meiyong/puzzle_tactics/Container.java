dd1027c74d7f8fc478eaa225e4f528e768238d38
Dinan Yin <steroidg@gmail.com>
2012-11-21 13:58:41 +1100

    javadoc changes

--- Puzzle Tactics/src/net/meiyong/puzzle_tactics/Container.java
+++ Puzzle Tactics/src/net/meiyong/puzzle_tactics/Container.java
@@ -7,9 +7,8 @@ import android.graphics.Canvas;
 import android.view.MotionEvent;
 
 /**
- * This is the Container class it contains buttons
+ * This is the base class for containers that can handle events
  * @author Dinan Yin
- *
  */
 public class Container {
 
@@ -19,6 +18,14 @@ public class Container {
 	protected int h; //height
 	protected PuzzleTacticsMainScreen mainScreen;
 	
+	/**
+	 * Constructor for Container class
+	 * @param x Centre position x of the container
+	 * @param y Centre position y of the container
+	 * @param w width of the container
+	 * @param h Height of the container
+	 * @param mainScreen the screen upon which this container is created
+	 */
 	public Container (int x, int y, int w, int h, PuzzleTacticsMainScreen mainScreen) {
 		this.x = x;
 		this.y = y;
@@ -28,72 +35,80 @@ public class Container {
 	}
 	
 	/**
-	 * Set the x position of the button
-	 * @param x Position x of the button
+	 * Set the x position of the container
+	 * @param x Position x of the container
 	 */
 	protected void setX (int x) {
 		this.x = x;
 	}
 	
 	/**
-	 * Set the y position of the button
-	 * @param y Position y of the button
+	 * Set the y position of the container
+	 * @param y Position y of the container
 	 */
 	protected void setY (int y) {
 		this.y = y;
 	}
 	
 	/**
-	 * Set the height of the button
-	 * @param x Height of the button
+	 * Set the height of the container
+	 * @param x Height of the container
 	 */
 	protected void setHeight (int h) {
 		this.h = h;
 	}
 	
 	/**
-	 * Set the width of the button
-	 * @param y Width of the button
+	 * Set the width of the container
+	 * @param y Width of the container
 	 */
 	protected void setWidth (int w) {
 		this.w = w;
 	}
 	
 	/**
-	 * Get the x position of the button
-	 * @return Position x of the button
+	 * Get the x position of the container
+	 * @return Position x of the container
 	 */
 	protected int getX () {
 		return x;
 	}
 	
 	/**
-	 * Get the y position of the button
-	 * @return Position y of the button
+	 * Get the y position of the container
+	 * @return Position y of the container
 	 */
 	protected int getY () {
 		return y;
 	}
 	
 	/**
-	 * Get the height of the button
-	 * @return Height of the button
+	 * Get the height of the container
+	 * @return Height of the container
 	 */
 	protected int getHeight () {
 		return h;
 	}
 	
 	/**
-	 * Get the width of the button
-	 * @return width of the button
+	 * Get the width of the container
+	 * @return width of the container
 	 */
 	protected int getWidth () {
 		return w;
 	}
 	
+	/**
+	 * Dummy method for event handling
+	 * @param event MotionEvent to handle 
+	 */
 	protected void handleEvent (MotionEvent event) {
 	}
 	
+	/**
+	 * Dummy method for the subclasses to draw the actual container
+	 * @param canvas Canvas to draw on
+	 */
 	protected void draw (Canvas canvas) {
 	}
 }
