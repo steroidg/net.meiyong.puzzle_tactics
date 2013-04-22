f8b96fbb63ece9a1ae2415fefb00b155446461fc
Dinan Yin <steroidg@gmail.com>
2013-04-22 22:07:26 +1000

    Handles event, renders navigationView

--- Puzzle Tactics/src/net/meiyong/puzzle_tactics/DaoMainScreen.java
+++ Puzzle Tactics/src/net/meiyong/puzzle_tactics/DaoMainScreen.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -19,11 +20,12 @@ import android.view.SurfaceView;
 public class DaoMainScreen extends SurfaceView implements SurfaceHolder.Callback {
 	private DaoMainThread mainThread;
 	private ControlPanel controlPanel;
+	private NavigationView navigationView;
 	private static final String TAG = DaoMainScreen.class.getSimpleName();
 	private int screenWidth;
 	private int screenHeight;
 	// Magic number of the size of each grid, always a square
-	private static final int gridUnit = 180;
+	private int gridUnit;
 	
 	/**
 	 * Constructor of PuzzleTacticsMainScreen class
@@ -54,14 +56,20 @@ public class DaoMainScreen extends SurfaceView implements SurfaceHolder.Callback
 		this.setThreadStatus(turnOn);
 		screenWidth=getWidth();
 		screenHeight=getHeight();
+		gridUnit=screenWidth/10;
 		
 		// Generate the control panel with buttons
 		int cpanel_x = screenWidth - gridUnit / 2;
 		int cpanel_y = screenHeight / 2;
 		int cpanel_width = gridUnit;
 		int cpanel_height = screenHeight;
-		controlPanel = new ControlPanel (cpanel_x, cpanel_y, cpanel_width, cpanel_height, this);
-//		navigationView = new NavigationView (screenWidth/2, screenHeight/2)
+		controlPanel = new ControlPanel (cpanel_x, cpanel_y, cpanel_width, cpanel_height, gridUnit, this);
+		
+		int navi_x = (screenWidth-cpanel_width)/2;
+		int navi_y = screenHeight/2;
+		int navi_width = screenWidth-cpanel_width;
+		int navi_height = screenHeight;
+		navigationView = new NavigationView (navi_x, navi_y, navi_width, navi_height, gridUnit, this);
 	}
 
 	/* (non-Javadoc)
@@ -80,9 +88,22 @@ public class DaoMainScreen extends SurfaceView implements SurfaceHolder.Callback
 		}
 	}
 	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		Log.d(TAG, "Coords: x=" + event.getX() + ",y=" + event.getY());
+		if ((event.getX() < (controlPanel.getX() + controlPanel.getWidth()/2) &&
+				event.getX() > (controlPanel.getX() - controlPanel.getWidth()/2)) && 
+				(event.getY() < (controlPanel.getY() + controlPanel.getHeight()/2) &&
+						event.getY() > (controlPanel.getY() - controlPanel.getHeight()/2))) {
+			controlPanel.handleEvent(event);
+		}
+		return true;
+	}
+	
 	protected void render(Canvas canvas) {
 		//Log.d(TAG,  "render");
 		canvas.drawColor(Color.WHITE);
+		navigationView.draw(canvas);
 		controlPanel.draw(canvas);
 	}
 	
