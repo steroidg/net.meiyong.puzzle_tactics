8c66119b9db1e2cfd85262eae7a98208cbbf48e5
Dinan Yin <steroidg@gmail.com>
2013-04-22 22:07:40 +1000

    Added close button, handle event

--- Puzzle Tactics/src/net/meiyong/puzzle_tactics/ControlPanel.java
+++ Puzzle Tactics/src/net/meiyong/puzzle_tactics/ControlPanel.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.Log;
+import android.view.MotionEvent;
 
 /**
  * This is the control panel of project Dao
@@ -18,22 +19,33 @@ public class ControlPanel extends Container {
 	private Paint paint;
 	private RectF rectf;
 	private static final String TAG = ControlPanel.class.getSimpleName();
+	private RoundButton closeButton;
 
 	/**
 	 * @param x
 	 * @param y
 	 * @param w
 	 * @param h
+	 * @param gridUnit
 	 * @param mainScreen
 	 */
-	protected ControlPanel(int x, int y, int w, int h, DaoMainScreen mainScreen) {
+	protected ControlPanel(int x, int y, int w, int h, int gridUnit, DaoMainScreen mainScreen) {
 		super(x, y, w, h, mainScreen);
 		Log.d(TAG, "Constructor: x=" + x + " y=" + y + " w=" + w + " h=" + h);
 		rectf = new RectF();
 		paint = new Paint();
-		// TODO Auto-generated constructor stub
+		closeButton = new RoundButton(x, h-gridUnit/2, gridUnit, gridUnit, Color.BLUE);
 	}
 
+	@Override
+	protected void handleEvent (MotionEvent event) {
+		if ((event.getX() < (closeButton.getX() + closeButton.getWidth()/2) &&
+				event.getX() > (closeButton.getX() - closeButton.getWidth()/2)) && 
+				(event.getY() < (closeButton.getY() + closeButton.getHeight()/2) &&
+						event.getY() > (closeButton.getY() - closeButton.getHeight()/2))) {
+			mainScreen.setThreadStatus(false);
+		}
+	}
 	
 	@Override
 	protected void draw (Canvas canvas) {
@@ -43,6 +55,7 @@ public class ControlPanel extends Container {
 		rectf.bottom = y + h/2;
 		rectf.left = x - w/2;
 		rectf.right = x + w/2;
-		canvas.drawOval(rectf, paint);
+		canvas.drawRect(rectf, paint);
+		closeButton.draw(canvas);
 	}
 }
